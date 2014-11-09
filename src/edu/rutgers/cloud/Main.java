8d99801ac79abd583af3a023fe5f3843e0c92dc7
SaumyaSoman <saumi89@gmail.com>
2014-11-09 17:19:28 -0500

    initial wordcount

---
+++ src/edu/rutgers/cloud/Main.java
@@ -0,0 +1,70 @@
+package edu.rutgers.cloud;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+        
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+public class Main {
+	
+	public static class SearchReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+		public void reduce(Text key, Iterator<IntWritable> values, Context context) 
+			      throws IOException, InterruptedException {
+			        int sum = 0;
+			        while (values.hasNext()) {
+			            sum += values.next().get();
+			        }
+			        context.write(key, new IntWritable(sum));
+			    }
+	}
+
+	public static class SearchMapper  extends Mapper<LongWritable, Text, Text, IntWritable> {
+		
+		private final static IntWritable one = new IntWritable(1);
+	    private Text word = new Text();
+	        
+	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+	        String line = value.toString();
+	        StringTokenizer tokenizer = new StringTokenizer(line);
+	        while (tokenizer.hasMoreTokens()) {
+	            word.set(tokenizer.nextToken());
+	            context.write(word, one);
+	        }
+	    }
+	}
+	
+	public static void main(String[] args) throws Exception {
+	    Configuration conf = new Configuration();
+	        
+	        Job job = new Job(conf);
+	    
+	        
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(IntWritable.class);
+	        
+	    job.setMapperClass(Main.SearchMapper.class);
+	    job.setReducerClass(Main.SearchReducer.class);
+	        
+	    job.setInputFormatClass(TextInputFormat.class);
+	    job.setOutputFormatClass(TextOutputFormat.class);
+	        
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	        
+	    job.setJarByClass(Main.class);
+	    job.waitForCompletion(true);
+	 }
+}
+
